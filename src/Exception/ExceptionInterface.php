904c0e0b8657f2c184af4c7ec83b542ec6081693
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:41:07 -0600

    Rewriting as Laminas Project package

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
- * @link      http://github.com/zendframework/zend-router for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-router for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-router/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-router/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Router\Exception;
+namespace Laminas\Router\Exception;
 
 interface ExceptionInterface
 {
