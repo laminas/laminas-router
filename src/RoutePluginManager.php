f01eeb5d5cab0c77f2128100446f4bd34cb90f1e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:09:46 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/RoutePluginManager.php
+++ src/RoutePluginManager.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-router for the canonical source repository
- * @copyright https://github.com/laminas/laminas-router/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-router/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace Laminas\Router;
