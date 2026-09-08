dd963a6c3f17e6304e9ebcb3511a2ed24e3f3550
fezfez <demonchaux.stephane@gmail.com>
2026-09-08 15:32:45 +0200

    fix following review
    Signed-off-by: fezfez <demonchaux.stephane@gmail.com>

--- src/RouteInterface.php
+++ src/RouteInterface.php
@@ -13,6 +13,7 @@ use Laminas\Stdlib\RequestInterface;
  *    type: class-string<RouteInterface>|non-empty-string,
  *    name?: non-empty-string,
  *    child_routes?: list<array<string, mixed>>,
+ *    ...
  *  }
  */
 interface RouteInterface
